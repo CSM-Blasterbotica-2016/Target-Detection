3c48713ed55e59c7bf5c0ffc9feb2c9b1541216b
https://mbaileyMines@bitbucket.org <mbailey@mines.edu>
2016-08-05 21:36:59 -0600

    Updates to 3/29 build

---
+++ build/CMakeFiles/FiducialTargeting.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/FiducialTargeting.dir/main.cpp.o"
+  "FiducialTargeting.pdb"
+  "FiducialTargeting"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/FiducialTargeting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
